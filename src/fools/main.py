8ed9d705acae1bf89b79bf80a4b1a13ec96fb834
abdmoh123 <abdhawisa@gmail.com>
2026-07-05 04:14:03 +0100

    feat: build app to a typer app
    Running the executable will be like using typer on the script.

--- src/fools/main.py
+++ src/fools/main.py
@@ -1,9 +1,19 @@
 """Main endpoint for the fools project."""
 
+import typer
 
-def main() -> None:
-    print("hello world")
+app = typer.Typer()
+
+
+@app.command()
+def main(name: str) -> None:
+    """Main command.
+
+    Args:
+        name: The name to say hello to.
+    """
+    print(f"Hello, {name}")
 
 
 if __name__ == "__main__":
-    main()
+    app()
